362893313c2e93c02502651318e10caf15efbadb
LejeuneKat <katheryne.lejeune@iCloud.com>
2025-11-20 08:05:37 -0800

    Agregue añadir item

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -27,9 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-LOGIN_URL= 'store/login/'
-LOGIN_REDIRECT_URL='/'
+LOGIN_URL = '/store/login/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
 # Application definition
 
 INSTALLED_APPS = [
